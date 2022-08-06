1fceedead9d497d288ec319496b90aecdfd3c812
onskulit <dislaty@mail.com>
2022-08-06 13:45:37 +0300

    refactor: refactor animation with requestAnimationFrame

--- src/utils/races.ts
+++ src/utils/races.ts
@@ -1,17 +1,23 @@
-const races: [number, ReturnType<typeof setInterval>][] = [];
+const races: [number, ReturnType<typeof requestAnimationFrame>][] = [];
 
 export const runRaceAnimation = (id: number, animationItem: HTMLElement, endpoint: number) => {
   const item = animationItem;
   let currentValue = +item.style.left.replace(/[^.0-9]/g, '');
-  const race = setInterval(() => {
-    if (item.offsetLeft >= endpoint) {
-      clearInterval(race);
-      races.splice(races.indexOf([id, race]), 1);
-      return;
+  let race = 0;
+  const animate = () => {
+    const racePare = races.find((pare) => pare[0] === id);
+    if (item.offsetLeft < endpoint) {
+      currentValue += 5;
+      item.style.left = `${currentValue}px`;
+      race = requestAnimationFrame(animate);
+      if (racePare) racePare[1] = race;
+    } else {
+      if (racePare) {
+        races.splice(races.indexOf(racePare), 1);
+      };
     }
-    currentValue += 5;
-    item.style.left = `${currentValue}px`;
-  }, 10);
+  };
+  race = requestAnimationFrame(animate);
   races.push([id, race]);
   console.log(races);
 };
@@ -19,7 +25,7 @@ export const runRaceAnimation = (id: number, animationItem: HTMLElement, endpoin
 export const cleanRaceAnimation = (id: number, animItem: HTMLElement, startPosition: string) => {
   const race = races.find((pare) => pare[0] === id);
   if (race) {
-    clearInterval(race[1]);
+    cancelAnimationFrame(race[1]);
     races.splice(races.indexOf(race), 1);
   }
   console.log(races);
