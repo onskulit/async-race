aaa195955320c0798c2d3f8afe0e3ce54e4ca1e5
onskulit <dislaty@mail.com>
2022-08-08 18:50:55 +0300

    feat: update basename

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename='/async-race'>
+    <BrowserRouter basename="/async-race">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
